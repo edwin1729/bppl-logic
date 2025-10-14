b7e164072f16e9dee582fb101bc41abddfe8255d
Edwin Fernando <ef322@ic.ac.uk>
2025-10-14 20:25:00 +0100

    begin denotation of terms

--- Bppl/Basic.lean
+++ Bppl/Basic.lean
@@ -25,24 +25,6 @@ abbrev var := String
 --  | score: te → te
 --  | normalize: te → te
 --
---mutual
---  inductive t where
---    | typed: t' → 𝔸 → t
---
---  inductive t' where
---    | ins: Nat → t → t' -- (i,t)
---    | case: t → (Nat → var → t) → t'
---    | unit : t'
---    | prod: t → t → t'
---    | proj: t → Nat → t'
---    -- | f(t)
---    | var: var → t'
---    | ret: t → t' -- return(t)
---    | lt: var → t → t' -- let x = t in u
---    | sample: t → t'
---    | score: t → t'
---    | normalize: t → t'
---end
 
 inductive 𝔸 where
   | R
@@ -52,51 +34,124 @@ inductive 𝔸 where
   | sum: 𝔸 → 𝔸 → 𝔸
   -- | sum (ι : Type*) [Countable ι] : (ι → 𝔸) → 𝔸 -- ⨆ 𝔸ᵢ
 
+mutual
+  structure t where
+    unty_te : t'
+    ty: 𝔸
+
+  -- could use higher order syntax. Which is perhaps cleaner atleast for the case arm.
+  -- But need to modufy how the context works a bit so for now just putting it on the programmer
+  inductive t' where
+    | inl: t → t' -- (i,t)
+    | inr: t → t' -- (i,t)
+    -- the 2nd and 3rd `t` is uses the `var` as a free variable
+    | case: t → var → t → t → t'
+    | unit : t'
+    | prod: t → t → t'
+    | proj: t → Nat → t'
+    -- | f(t)
+    | var: var → t'
+    | ret: t → t' -- return(t)
+    | lt: var → t → t' -- let x = t in u
+    | sample: t → t'
+    | score: t → t'
+    | normalize: t → t'
+end
+
 /-- Typed terms of a Baysian probabilistic programming language.
 The types are only required at binding sites: `let` and `case` -/
-inductive t where
-  | ins: Nat → t → t -- (i,t)
-  | case: t → (Nat → var × 𝔸 → t) → t
-  | unit : t
-  | prod: t → t → t
-  | proj: t → Nat → t
-  | var: var → t
-  | ret: t → t -- return(t)
-  | lt: var × 𝔸 → t → t -- let x = t in u
-  | sample: t → t
-  | score: t → t
-  | normalize: t → t
+--inductive t where
+--  | ins: Nat → t → t -- (i,t)
+--  | case: t → (Nat → var × 𝔸 → t) → t
+--  | unit : t
+--  | prod: t → t → t
+--  | proj: t → Nat → t
+--  | var: var → t
+--  | ret: t → t -- return(t)
+--  | lt: var × 𝔸 → t → t -- let x = t in u
+--  | sample: t → t
+--  | score: t → t
+--  | normalize: t → t
 
 -- def measure_iunion (ι : Type*) (f: ι → Σ T: Type, MeasurableSpace T) :
 --   MeasurableSpace T₁ ⊕ T₂ := sorry
 
 -- marked as noncomputable because of Giry
-noncomputable def type_denote (ty: 𝔸): Σ (T: Type), MeasurableSpace T :=
+noncomputable def type_denote' (ty : 𝔸) : Σ (T: Type), MeasurableSpace T :=
   match ty with
   | 𝔸.R => ⟨ℝ, borel ℝ⟩
   | 𝔸.P ty' =>
-    let ⟨ty, _⟩ := type_denote ty'
+    let ⟨ty, _⟩ := type_denote' ty'
     let foo := MeasCat.Giry.obj (MeasCat.of ty)
     ⟨foo, foo.str⟩
   | 𝔸.unit => ⟨PUnit, PUnit.instMeasurableSpace⟩ -- unit
   | 𝔸.prod ty₁' ty₂' =>
-    let ⟨ty₁, _⟩ := type_denote ty₁'
-    let ⟨ty₂, _⟩ := type_denote ty₂'
+    let ⟨ty₁, _⟩ := type_denote' ty₁'
+    let ⟨ty₂, _⟩ := type_denote' ty₂'
     ⟨ty₁ × ty₂, (inferInstance : MeasurableSpace (ty₁ × ty₂))⟩
   | 𝔸.sum ty₁' ty₂' =>
-    let ⟨ty₁, _⟩ := type_denote ty₁'
-    let ⟨ty₂, _⟩ := type_denote ty₂'
+    let ⟨ty₁, _⟩ := type_denote' ty₁'
+    let ⟨ty₂, _⟩ := type_denote' ty₂'
     ⟨ty₁ ⊕ ty₂, (inferInstance : MeasurableSpace (ty₁ ⊕ ty₂))⟩
+
+noncomputable def type_denote (ty' : 𝔸) : MeasCat :=
+  let ⟨ty, _⟩ := type_denote' ty'
+  MeasCat.of ty
 
 -- abbrev denotation: -- (var → 𝔸) → ()
 
-def type_prod (tys: List Type): Type :=
+def type_prod (tys : List MeasCat): MeasCat :=
   match tys with
-  | [] => Unit
+  | [] => MeasCat.of PUnit
   | [ty] => ty
-  | ty::tys => ty × type_prod tys
+  | ty::tys => MeasCat.of (ty × type_prod tys)
 
-inductive out (α : MeasCat) where
-  | det: Σ (β : MeasCat) (f: α → β), Measurable f
+abbrev denotation_ty (α β : MeasCat) :=
+  {f : α → β // Measurable f} ⊕ Kernel α β
 
-def denote (vs: List (var × t × 𝔸)) (te: t) : Except String ()
+-- get deterministic denotation or fail
+def get_det {α β : MeasCat} (den : denotation_ty α β) (err_msg : String)
+  : Except String {f : α → β // Measurable f} :=
+  den.elim (fun det ↦ Except.ok det) (fun _ ↦ Except.error err_msg)
+
+-- get probabilistic denotation or fail
+def get_prob {α β : MeasCat} (den : denotation_ty α β) (err_msg : String)
+  : Except String (Kernel α β) :=
+  den.elim (fun _ ↦ Except.error err_msg) (fun prob ↦ Except.ok prob)
+
+-- I don't aim to type check here. I do a sort of optimistic type checking.
+-- (Probably called dynamic type checking actually)
+-- Invalidly typed terms recieve undefined semantics instead of failing as expected
+-- A type checker is best added as a separate step later
+def denote (vs: List (var × t × 𝔸)) (te : t) : Except String
+  (denotation_ty (type_prod ((List.map (fun (a,b,c) ↦ (type_denote c)) vs))) (type_denote te.ty)) :=
+  match te.unty_te with
+  | everyhting => sorry
+  --| t'.inl te => do
+  --  let te_denotation ← (denote vs te)
+  --  let measurable_fn ← get_det te_denotation "Only deterministic values allowed in an injection"
+  --  pure (Sum.inl ((fun x ↦ Sum.inl x) ∘ measurable_fn))
+  --  -- pure ((fun x ↦ (0, x)) ∘ measurable_fn)
+  --  -- pure (false, te'') -- arbitrary choice to represent left with false
+  --| t'.inl te => do
+  --  let te' := get_det (denote vs te) "Only deterministic values allowed in an injection"
+  --  return (true, te') -- arbitrary choice to represent right with true
+  -- This arm is interesting if typechecking and not using the binary sum type and
+  -- using the countably infinite sum type from the paper
+  --| t'.case te name left right => do
+  --  if
+  --  let foo := denote vs te
+  --| t'.unit => t'
+  --| t'.prod: t → t → t'
+  --| t'.proj: t → Nat → t'
+  ----t'. | f(t)
+  --| t'.var: var → t'
+  --| t'.ret: t → t' -- return(t)
+  --| t'.lt: var → t → t' -- let x = t in u
+  --| t'.sample: t → t'
+  --| t'.score: t → t'
+  --| t'.normalize: t → t'
+
+
+-- files to look into for the disintegration theorem
+-- Basic, density, standard borel, unique
