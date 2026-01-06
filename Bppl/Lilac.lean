ebf56815504f6e4ed901c237120f7f3e06941656
Edwin Fernando <ef322@ic.ac.uk>
2026-01-06 13:21:12 +0000

    remove/sorry proofs, instantiate BI

--- Bppl/Lilac.lean
+++ Bppl/Lilac.lean
@@ -1,4 +1,8 @@
 import Iris.BI.BIBase
+import Iris.BI
+import Iris.Algebra.OFE
+import Iris.Std.Equivalence
+
 import Mathlib.Data.PFun
 import Mathlib.MeasureTheory.MeasurableSpace.Defs
 import Mathlib.MeasureTheory.Measure.Dirac
@@ -14,7 +18,7 @@ section KRM
 -- set_option quotPrecheck false
 /-- Partial Commutative Monoid -/
 class PCM (α : Type*) extends One α where
-  binop : α → α →. α -- partial binary operation
+  binop : α → α → Option α -- partial binary operation
 
 notation a:arg " • " b:arg => PCM.binop a b
 
@@ -26,7 +30,7 @@ class PCM' (α : Type*) extends PCM α where
     (binop a b) >>= (fun ab => (binop ab c)) =
     (binop b c) >>= (fun bc => (binop a bc))
 
-class KRM (α : Type*) extends PCM α, PartialOrder α where
+class KRM (α : Type*) extends PCM' α, PartialOrder α where
   ge_mul_mono : ∀ x x' y y' p' : α,
     x ≤ x' → y ≤ y' →
     (x' • y' = some p') → ∃ p, (x • y = some p) ∧  p ≤ p'
@@ -35,33 +39,6 @@ class KRM (α : Type*) extends PCM α, PartialOrder α where
 -- We need a type of probability spaces parametrized by given carrier type α
 
 def ProbabilitySpace (α : Type*) := Σ (m: MeasurableSpace α), @ProbabilityMeasure α m
-
-namespace ProbabilitySpace
-
-variable {α : Type*} {m m0 : MeasurableSpace α}
-
--- to mathlib?
--- maybe I should use `Measure.trim` instead of copying and adapting its definition
-noncomputable
-def trim
-  (μ : @ProbabilityMeasure α m0) (hm : m ≤ m0) : @ProbabilityMeasure α m :=
-    ⟨Measure.trim μ hm,
-      ⟨by
-        have eq_univ : μ Set.univ = Measure.trim μ hm Set.univ := by
-          simp only [ProbabilityMeasure.coeFn_univ, ENNReal.coe_one, MeasurableSet.univ,
-            toMeasure_apply, Measure.coe_toOuterMeasure, measure_univ, Measure.trim]
-        simp_all only [ProbabilityMeasure.coeFn_univ, ENNReal.coe_one, Measure.trim]
-    ⟩⟩
-
--- why is μ.trim syntax not working?
-lemma trim_measurableSet_eq {s : Set α} {μ : @ProbabilityMeasure α m0}
-  (hm : m ≤ m0) (hs : @MeasurableSet α m s) : trim μ hm s = μ s := by
-    have foo := @MeasureTheory.trim_measurableSet_eq _ _ _ μ.1 _ hm hs
-    rw [trim]
-    simp_all only [ProbabilityMeasure.val_eq_to_measure, ProbabilityMeasure.mk_apply]
-    rfl
-
-end ProbabilitySpace
 
 open Classical
 
@@ -80,13 +57,6 @@ section indep_comb
 
 variable {α : Type*} (ℰ ℱ 𝒢 𝒢': ProbabilitySpace α)
 
--- At the moment I talk explicitly about properties of combinations of measures
--- (uniqueness/existence). These are trivial for spaces. However it may be cleaner later on
--- to talk of the space as a whole instead
-
--- TODO fix the need to do `toMeasurableSpace`. It's annoying
--- notation "∀ " s:arg " ∈ᵐ " P:arg ", " body:arg => ∀ s, MeasurableSet[P] s → body
-
 -- why does the R.volume part work? I think the `measure` is a garbage value for sets which
 -- are not measurable. But of course this is fine (cause the prop would just be false in
 -- garbage value case)
@@ -103,26 +73,12 @@ def indep_comb_measure' (𝒢 : ProbabilitySpace α) (ρ : @ProbabilityMeasure �
 
 def existence_cond : Prop := ∃ ρ, indep_comb_measure ℰ ℱ ρ
 
-def indep_comb : Prop := ∃ (h : ℰ.1 ⊔ ℱ.1 = 𝒢.1),
-  indep_comb_measure ℰ ℱ (ProbabilitySpace.trim 𝒢.2 (le_of_eq h))
-
--- What I want to do is t
-
--- Lemma 2.3 (independent combinations are unique)
--- use the π-λ-theorem here
-lemma uniqueness_indep_comb (h₁ : indep_comb ℰ ℱ 𝒢) (h₂ : indep_comb ℰ ℱ 𝒢') : 𝒢 = 𝒢'
-  := sorry
-
-lemma uniqueness_indep_comb' (ρ₁ ρ₂ : @ProbabilityMeasure α (ℰ.1 ⊔ ℱ.1))
-  (hρ₁ : indep_comb_measure ℰ ℱ ρ₁) (hρ₂ : indep_comb_measure ℰ ℱ ρ₂) : ρ₁ = ρ₂
-  := sorry
-
 end indep_comb
 
 noncomputable instance instPCM {α : Type*} [nonempty : Nonempty α] : PCM (ProbabilitySpace α) where
   binop ℰ ℱ := if h : existence_cond ℰ ℱ
-    then Part.some ⟨ℰ.1 ⊔ ℱ.1, Classical.choose h⟩
-    else Part.none
+    then some ⟨ℰ.1 ⊔ ℱ.1, Classical.choose h⟩
+    else none
 
 -- lemma binop_indep {α : Type*} [nonempty : Nonempty α] (ℰ ℱ : PrSp α) : indep_comb ℰ ℱ (ℰ • ℱ)
 
@@ -130,82 +86,88 @@ lemma inter_diff_space {α : Type*} {m m0 : MeasurableSpace α} {s : Set α} (hm
   : @MeasurableSet α m0 s := by
     measurability
 
-
 noncomputable instance instPCM' {α : Type*} [nonempty : Nonempty α] : PCM' (ProbabilitySpace α) where
-  one_mul 𝒢 := by
-
-    let one: ProbabilitySpace α := 1 -- can't seem to use dot syntax for 1
-    have eq_inf_one: one.1 ⊔ 𝒢.1 = 𝒢.1 := by
-      simp only [sup_eq_right]
-      exact bot_le
-    let ρ := ProbabilitySpace.trim 𝒢.2 (le_of_eq eq_inf_one)
-    -- The independent combination exists by case analysis on the σ-algebra of `1`
-    have ρ_indep_comb : indep_comb_measure 1 𝒢 ρ := by
-      intro e f he hf
-      rw [measurableSet_bot_iff] at he
-      cases he with
-      | inl he_empty => measurability
-      | inr he_univ =>
-        subst he_univ
-        simp_all only [Set.univ_inter, ProbabilityMeasure.coeFn_univ, one_mul,
-          ProbabilitySpace.trim_measurableSet_eq, ρ]
-
-    let existence: existence_cond 1 𝒢 := ⟨ρ, ρ_indep_comb⟩
-
-    simp only [PCM.binop, Part.coe_some]
-    rw [dite_cond_eq_true (eq_true existence), Part.some_inj]
-    let : indep_comb 1 𝒢 𝒢 := sorry
-    -- have :
-
-    let that : indep_comb 1 𝒢 (⟨one.1 ⊔ 𝒢.fst, choose existence⟩)
-      := by
-      rw [indep_comb]
-      simp only
-      use rfl
-      rw [indep_comb_measure]
-      intro E F hE hF
-
-      -- rw [trim_measurableSet_eq ]
-      have : (ProbabilitySpace.trim (choose existence) (indep_comb._proof_1 1 𝒢 ⟨one.fst ⊔ 𝒢.fst, choose existence⟩
-  ((Iff.of_eq (Eq.refl (one.1 ⊔ 𝒢.fst = one.fst ⊔ 𝒢.fst))).mpr rfl))) (E ∩ F) = (choose existence) (E ∩ F) :=
-        by
-          apply ProbabilitySpace.trim_measurableSet_eq
-          refine MeasurableSet.inter ?_ ?_
-          · exact inter_diff_space (by exact le_sup_left) hE
-          · exact inter_diff_space (by exact le_sup_of_le_right fun s a ↦ a) hF
-
-      rw [this]
-      apply choose_spec existence
-      · exact hE
-      · exact hF
-
-    apply uniqueness_indep_comb 1 𝒢
-    · exact that
-    · exact this
-
-    -- This is from the design where the uniqueness result works at the granularity of measures.
-    -- I think this would remove some bloat from the proof, and I would like to go back to it at
-    -- some point
-
-    -- have : ⊥ ⊔ 𝒢.fst = 𝒢.fst := by exact eq_inf_one
-    -- rw [this]
-
-    -- have foo (ρ': @ProbabilityMeasure α (one ⊔ 𝒢)) (heq: ρ = ρ'): ⟨one ⊔ 𝒢, ρ'⟩ =  := sorry
-    -- let uniqueness := uniqueness_indep_comb 1 𝒢 (instPCM.binop 1 𝒢).volume ρ
-
-    -- The probability measure is precisely that of `P.volume`, because the combination's
-    -- σ-algebra is just the same as `P`'s. But in general the probability measure is defined
-    -- by the axiom of choice. So the only way is to use 1) the uniqueness of the combination's
-    -- measure (via π-λ-theorem), and 2) show that `P`'s measure is a satisfactory instance
+  one_mul 𝒢 := sorry
   mul_one := sorry
   comm := sorry
   assoc := sorry
 
+noncomputable instance instPartialOrder {α : Type*} : PartialOrder (ProbabilitySpace α) where
+  le := sorry
+  le_refl := sorry
+  le_trans := sorry
+  le_antisymm := sorry
 
--- (m : ∀ s : Set α, MeasurableSet s → ℝ≥0∞)
--- instance instPartialOrder {α : Type*} : PartialOrder (ProbabilitySpace α) where
-
--- instance instKRM {α : Type*} : KRM (ProbabilitySpace α) where
---   ge_mul_mono := sorry
+noncomputable instance instKRM {α : Type*} [nonempty : Nonempty α] : KRM (ProbabilitySpace α) where
+  ge_mul_mono := sorry
 
 end KRM
+
+open Iris.BI Iris
+
+abbrev PROP (α : Type*) [nonempty : Nonempty α] := ProbabilitySpace α → Prop
+
+instance instBIBase {α : Type*} [nonempty : Nonempty α]: BIBase (PROP α ) where
+  Entails P Q      := ∀ σ, P σ → Q σ
+  emp            σ := σ = 1
+  pure φ         _ := φ
+  and P Q        σ := P σ ∧ Q σ
+  or P Q         σ := P σ ∨ Q σ
+  imp P Q        σ := P σ → Q σ
+  sForall Ψ      σ := ∀ p, Ψ p → p σ
+  sExists Ψ      σ := ∃ p, Ψ p ∧ p σ
+  sep P Q        σ := ∃ σ1 σ2 : ProbabilitySpace α, σ1 • σ2 = some σ ∧ P σ1 ∧ Q σ2
+  wand P Q       σ := ∀ σ' : ProbabilitySpace α, (h : (σ • σ').isSome) → P σ' → Q ((σ • σ').get h)
+  -- could we do better than this? Identify what more is persistent/affine
+  -- wasn't BaSL partially affine? What does that mean?
+  persistently P _ := P 1
+  later P        σ := P σ -- there is no step indexing
+
+instance {α : Type*} [nonempty : Nonempty α] : COFE (PROP α) := COFE.ofDiscrete Eq equivalence_eq
+
+instance instBI {α : Type*} [nonempty : Nonempty α] : BI (PROP α) where
+  equiv_iff := sorry
+  entails_preorder := sorry
+  and_ne := sorry
+  or_ne := sorry
+  imp_ne := sorry
+  sForall_ne := sorry
+  sExists_ne := sorry
+  sep_ne := sorry
+  wand_ne := sorry
+  persistently_ne := sorry
+  later_ne := sorry
+  pure_intro := sorry
+  pure_elim' := sorry
+  and_elim_l := sorry
+  and_elim_r := sorry
+  and_intro := sorry
+  or_intro_l := sorry
+  or_intro_r := sorry
+  or_elim := sorry
+  imp_intro := sorry
+  imp_elim := sorry
+  sForall_intro := sorry
+  sForall_elim := sorry
+  sExists_intro := sorry
+  sExists_elim := sorry
+  sep_mono := sorry
+  emp_sep := sorry
+  sep_symm := sorry
+  sep_assoc_l := sorry
+  wand_intro := sorry
+  wand_elim := sorry
+  persistently_mono := sorry
+  persistently_idem_2 := sorry
+  persistently_emp_2 := sorry
+  persistently_and_2 := sorry
+  persistently_sExists_1 := sorry
+  persistently_absorb_l := sorry
+  persistently_and_l := sorry
+  later_mono := sorry
+  later_intro := sorry
+  later_sForall_2 := sorry
+  later_sExists_false := sorry
+  later_sep := sorry
+  later_persistently := sorry
+  later_false_em := sorry
