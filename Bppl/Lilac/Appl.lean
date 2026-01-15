3e4e22ed97d57ebf269636d1ec5af5d069b60dd7
Edwin Fernando <ef322@ic.ac.uk>
2026-01-15 11:09:40 +0000

    progress in denotation of terms in appl

--- Bppl/Lilac/Appl.lean
+++ Bppl/Lilac/Appl.lean
@@ -1,5 +1,5 @@
 import Mathlib
-
+import Mathlib.Probability.ProbabilityMassFunction.Constructions
 /-! The probabilistic programming language without the observe/score primitive.
 As defined in Lilac.
 
@@ -38,12 +38,12 @@ theorem List.TProd.measurable_get [∀ i, MeasurableSpace (β i)] (mem : Member 
   | tail _ ih => exact ih.comp measurable_snd
 
 class Denotational (Ty : Type u) where
-  denote : Ty → Type
+  den : Ty → Type
 
-notation "⟦" t "⟧" => Denotational.denote t
+notation "⟦" t "⟧" => Denotational.den t
 
 class DenotationalMeas (Ty : Type u) extends Denotational Ty where
-  instMeasurable : ∀ t, MeasurableSpace (denote t)
+  instMeasurable : ∀ t, MeasurableSpace (den t)
 
 
 instance instMeasurableSpaceDenotation {Ty : Type u} [d : DenotationalMeas Ty] (t : Ty) :
@@ -63,6 +63,7 @@ open MeasureTheory in
 /-- Optionally provide non-standard (co)domain σ-algebra -/
 notation α " -m[ " σ₁ ", " σ₂ "]→ " β => {f: α → β // Measurable[σ₁, σ₂] f}
 
+open NNReal MeasureTheory PMF Measurable ProbabilityTheory
 namespace Appl
 -- Lilac A.1 and A.2 (appendix)
 inductive Ty where
@@ -75,17 +76,17 @@ inductive Ty where
 
 scoped notation ty₁ " × " ty₂ => Ty.prod ty₁ ty₂
 
-structure Arith where
-  plus : Arith
-  minus : Arith
-  mult : Arith
-  div : Arith
-  exp : Arith
+inductive Arith where
+  | add : Arith
+  | sub : Arith
+  | mul : Arith
+  | div : Arith
+  | pow : Arith
 
-structure Cmp where
-  lt : Cmp
-  le : Cmp
-  eq : Cmp
+inductive Cmp where
+  | lt : Cmp
+  | le : Cmp
+  | eq : Cmp
 
 inductive Term : List Ty → Ty → Type
   | var : Member ty ctx → Term ctx ty
@@ -97,7 +98,7 @@ inductive Term : List Ty → Ty → Type
   | T : Term ctx .bool
   | F : Term ctx .bool
   | ite : Term ctx .bool → Term ctx ty → Term ctx ty → Term ctx ty
-  | flip : Set.Icc 0 1 → Term ctx (.G .bool)
+  | flip : (p : ℝ≥0) → (h : p ≤ 1) → Term ctx (.G .bool)
   | r : ℝ → Term ctx .real
   | arith : Arith → Term ctx .real → Term ctx .real → Term ctx .real
   | cmp : Cmp → Term ctx .real → Term ctx .real → Term ctx .bool
@@ -110,37 +111,80 @@ inductive Term : List Ty → Ty → Type
 noncomputable section
 -- Most of the MeasurableSpace instance provided in
 -- Mathlib.MeasureTheory.MeasurableSpace.Instances
-@[reducible] def Ty.denote : Ty → MeasCat
-  | prod ty₁ ty₂ => .of (ty₁.denote × ty₂.denote)
+@[reducible] def Ty.den : Ty → MeasCat
+  | prod ty₁ ty₂ => .of (ty₁.den × ty₂.den)
   | bool => .of Bool
   | real => .of ℝ
-  | exp n ty => .of (∀ (_ : Fin n), ty.denote) -- using MeasurableSpace.pi
+  | exp n ty => .of (∀ (_ : Fin n), ty.den) -- using MeasurableSpace.pi
   | index => .of ℕ
-  | G ty => MeasCat.Measure.obj (ty.denote) --: Ty → Ty
+  | G ty => MeasCat.Measure.obj (ty.den) --: Ty → Ty
+
+-- Is this already defined somewhere
+-- also is there a more concise way to define measurable functions?
+@[reducible] def Arith.den : Arith → (ℝ × ℝ) -m→ ℝ
+  | add => ⟨fun (x,y) ↦ x + y, measurable_add⟩
+  | sub => ⟨fun (x,y) ↦ x - y, measurable_sub⟩
+  | mul => ⟨fun (x,y) ↦ x * y, measurable_mul⟩
+  | div => ⟨fun (x,y) ↦ x / y, measurable_div⟩
+  | pow => ⟨fun (x,y) ↦ x ^ y, measurable_pow⟩
+
+def foo (p: ℝ × ℝ) : Bool := p.1 < p.2
+
+lemma foop : Measurable (foo) := by
+  rw [Measurable]
+  intro t ht
+  have h : t = ∅ ∨ t = {true} ∨ t = {false} ∨ t = Set.univ := by
+    rcases em (true ∈ t) with ht' | ht' <;> rcases em (false ∈ t) with hf' | hf'
+    · right; right; right; ext b; cases b <;> simp [*]
+    · right; left; ext b; cases b <;> simp [*]
+    · right; right; left; ext b; cases b <;> simp [*]
+    · left; ext b; cases b <;> simp [*]
+  rcases h with rfl | rfl | rfl | rfl
+  · exact MeasurableSet.empty
+  · have : foo ⁻¹' {true} = {p : ℝ × ℝ | p.1 < p.2} := by ext p; simp [foo]
+    rw [this]
+    exact isOpen_lt continuous_fst continuous_snd |>.measurableSet
+  · have : foo ⁻¹' {false} = {p : ℝ × ℝ | p.1 < p.2}ᶜ := by ext p; simp [foo, not_lt]
+    rw [this]
+    exact (isOpen_lt continuous_fst continuous_snd).measurableSet.compl
+  · exact MeasurableSet.univ
+
+-- surely it shouldn't be this hard to show that the comparison operators are measurable
+-- Either generalise over all of them or make a tactic
+@[reducible] def Cmp.den : Cmp → (ℝ × ℝ) -m→ Bool
+  | lt => ⟨fun p ↦ p.1 < p.2, foop⟩
+  | le => ⟨fun p ↦ p.1 ≤ p.2, sorry⟩
+  | eq => ⟨fun p ↦ p.1 = p.2, sorry⟩
 
 instance : DenotationalMeas Ty where
-  denote ty := ↑ty.denote
-  instMeasurable ty := ty.denote.2
+  den ty := ↑ty.den
+  instMeasurable ty := ty.den.2
 
 -- Sadly Classes can't be annotate without reducible to. So the notation `⟦⟧` can't
 -- may used through a type class and still be made reducible? Investigate
-@[simp] def Term.denote : Term ctx ty → List.TProd (⟦·⟧) ctx -m→ ty.denote
+-- and at the same time I struggle to get type class inference to notice the M
+@[simp] def Term.den : Term ctx ty → List.TProd (⟦·⟧) ctx -m→ ty.den
   | var mem => ⟨fun env ↦ env.get mem, List.TProd.measurable_get mem⟩
-  | ret X => ⟨fun env ↦ .dirac (X.denote.1 env),
-      MeasureTheory.Measure.measurable_dirac.comp X.denote.2⟩
+  | ret X => ⟨fun env ↦ .dirac (X.den.1 env),
+      MeasureTheory.Measure.measurable_dirac.comp X.den.2⟩
   | bind M N => sorry -- the arguemnt `X` is the extra term in `N`'s context
-  | pair M N => sorry
-  | fst M => sorry
-  | snd M => sorry
-  | T => sorry
-  | F => sorry
-  | ite M N O => sorry
-  | flip p => sorry
-  | r x => sorry
-  | arith op M N => sorry
-  | cmp op M N => sorry
-  | unif01 => sorry --: Term ctx (.G .real)
-  | vect f => sorry --: (Fin n → Term ctx ty) → Term ctx (.exp n ty)
+  | pair M N => ⟨fun env ↦ (M.den.1 env, N.den.1 env), Measurable.prod M.den.2 N.den.2⟩
+  | fst M => ⟨fun env ↦ (M.den.1 env).fst, Measurable.fst M.den.2⟩
+  | snd M => ⟨fun env ↦ (M.den.1 env).snd, Measurable.snd M.den.2⟩
+  | T => ⟨fun env ↦ true, measurable_const⟩
+  | F => ⟨fun env ↦ false, measurable_const⟩
+  | ite P M N => ⟨fun env ↦ if P.den.1 env then M.den.1 env else N.den.1 env,
+      -- This is just a slightly finicky sorry, to show the function translating from bool to Prop
+      -- is Measurable. Just need that as an additional lemma
+      Measurable.ite (sorry) M.den.2 N.den.2⟩
+  | flip p hp => ⟨fun _ ↦ (bernoulli p hp).toMeasure, measurable_const⟩
+  | r x => ⟨fun _ ↦ x, measurable_const⟩
+  | arith op M N => ⟨fun env ↦ op.den.1 (M.den.1 env, N.den.1 env),
+    (op.den.2).comp (M.den.2.prod N.den.2)⟩
+  | cmp op M N => ⟨fun env ↦ op.den.1 (M.den.1 env, N.den.1 env),
+    (op.den.2).comp (M.den.2.prod N.den.2)⟩
+  | unif01 => ⟨fun _ ↦ uniformOn (Set.Icc (0: ℝ) 1), measurable_const⟩
+  | vect f => ⟨ , ⟩  --sorry --: (Fin n → Term ctx ty) → Term ctx (.exp n ty)
   | index N M => sorry -- : Term ctx .index → Term ctx (.exp n ty) → Term ctx ty
   -- No need to specify name for `i`: index and `X`: A, since we're using De brujin indices
   | «for» n Mᵢ Mₛ => sorry -- : ℕ → Term ctx ty → Term (index :: ty :: ctx) (.G ty) → Term ctx (.G ty)
